d6452d855295e3fb4aae1bc854d8ce9b3f40d9e9
kmhasan <kmhasan@SEUPC0719>
2015-10-11 09:51:25 +0600

    Calculator UI done

--- Section 1/Casio Calculator/src/casio/calculator/FXMLDocumentController.java
+++ Section 1/Casio Calculator/src/casio/calculator/FXMLDocumentController.java
@@ -10,26 +10,37 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author kmhasan
  */
 public class FXMLDocumentController implements Initializable {
-    
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private TextField displayField;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void handleOneAction(ActionEvent event) {
+        String oldText = displayField.getText();
+        String newText = oldText + "1";
+        displayField.setText(newText);
+    }
+
+    @FXML
+    private void handleDigitAction(ActionEvent event) {
+        String oldText = displayField.getText();
+        // We will talk about import and casting in next class
+        Button b = (Button) event.getSource();
+        String newText = oldText + b.getText();
+        displayField.setText(newText);
+    }
     
 }
