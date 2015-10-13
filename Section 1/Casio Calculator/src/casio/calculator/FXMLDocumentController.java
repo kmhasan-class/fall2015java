32a2a418bd49ab55590f7ce60db566e6734e685c
kmhasan <kmhasan@SEUPC0719>
2015-10-13 12:49:29 +0600

    Add and Subtract operation done

--- Section 1/Casio Calculator/src/casio/calculator/FXMLDocumentController.java
+++ Section 1/Casio Calculator/src/casio/calculator/FXMLDocumentController.java
@@ -10,15 +10,18 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 
 /**
  *
  * @author kmhasan
  */
 public class FXMLDocumentController implements Initializable {
+    double number1;
+    String operation;
+    
     @FXML
     private TextField displayField;
     
@@ -27,7 +30,6 @@ public class FXMLDocumentController implements Initializable {
         // TODO
     }    
 
-    @FXML
     private void handleOneAction(ActionEvent event) {
         String oldText = displayField.getText();
         String newText = oldText + "1";
@@ -41,6 +43,36 @@ public class FXMLDocumentController implements Initializable {
         Button b = (Button) event.getSource();
         String newText = oldText + b.getText();
         displayField.setText(newText);
+
+    }
+
+    @FXML
+    private void handleAddAction(ActionEvent event) {
+        String oldText = displayField.getText();
+        number1 = Double.parseDouble(oldText);
+        operation = "ADD";
+        displayField.setText("");
+    }
+
+    @FXML
+    private void handleSubtractOperation(ActionEvent event) {
+        String oldText = displayField.getText();
+        number1 = Double.parseDouble(oldText);
+        operation = "SUB";
+        displayField.setText("");
+    }
+
+    @FXML
+    private void handleEqualAction(ActionEvent event) {
+        String oldText = displayField.getText();
+        double number2 = Double.parseDouble(oldText);
+        double result = 0.0;
+        // I'll explain why we use .equals instead of == later
+        if (operation.equals("ADD"))
+            result = number1 + number2;
+        else if (operation.equals("SUB"))
+            result = number1 - number2;
+        displayField.setText("" + result);
     }
     
 }
