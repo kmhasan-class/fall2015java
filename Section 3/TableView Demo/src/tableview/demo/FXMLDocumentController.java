bffac7267b42bd5a325598ee4c3af1b8445f2925
kmhasan <kmhasan@SEUPC0719>
2015-11-23 09:25:27 +0600

    TableView Demo

---
+++ Section 3/TableView Demo/src/tableview/demo/FXMLDocumentController.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tableview.demo;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+/**
+ *
+ * @author kmhasan
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private ListView<FoodItem> itemList;
+    @FXML
+    private TableView<FoodItem> itemTable;
+    ObservableList<FoodItem> items;
+    @FXML
+    private TableColumn<FoodItem, String> itemNameColumn;
+    @FXML
+    private TableColumn<FoodItem, Number> itemPriceColumn;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        items = FXCollections.observableArrayList();
+        
+        itemList.setItems(items);
+        itemTable.setItems(items);
+        
+        itemNameColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getItemName()));
+        itemPriceColumn.setCellValueFactory(data -> new SimpleDoubleProperty(data.getValue().getPrice()));
+        
+        String DB_URL = "jdbc:mysql://172.17.0.119/fall2015db";
+        String DB_USER = "cse2015fall2015";
+        String DB_PASS = "fall2015";
+        
+        
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+            Statement statement = connection.createStatement();
+            String query = "select * from FoodItem";
+            ResultSet resultSet = statement.executeQuery(query);
+            
+            while (resultSet.next()) {
+                String itemName = resultSet.getString("itemName");
+                String category = resultSet.getString("catagory");
+                int itemCode = resultSet.getInt("itemCode");
+                String description = resultSet.getString("description");
+                double price = resultSet.getDouble("price");
+
+                FoodItem item = new FoodItem(itemCode, itemName, category, description, price);
+                items.add(item);
+            }
+            
+            for (FoodItem f: items)
+                System.out.printf("%s\n", f);
+        } catch (SQLException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }    
+    
+}
---
+++ Section 3/TableView Demo/src/tableview/demo/FoodItem.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tableview.demo;
+
+/**
+ *
+ * @author kmhasan
+ */
+public class FoodItem {
+    private int itemCode;
+    private String itemName;
+    private String category;
+    private String description;
+    private double price;
+
+    public FoodItem(int itemCode, String itemName, String category, String description, double price) {
+        this.itemCode = itemCode;
+        this.itemName = itemName;
+        this.category = category;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getItemCode() {
+        return itemCode;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "FoodItem{" + "itemCode=" + itemCode + ", itemName=" + itemName + ", category=" + category + ", description=" + description + ", price=" + price + '}';
+    }
+    
+    
+}
