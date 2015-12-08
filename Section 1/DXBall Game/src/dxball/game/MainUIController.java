be00236de622924632c0308b67c9f2a4037da634
kmhasan <kmhasan@SEUPC0719>
2015-12-08 09:53:10 +0600

    DXBall Game

--- Section 1/DXBall Game/src/dxball/game/MainUIController.java
+++ Section 1/DXBall Game/src/dxball/game/MainUIController.java
@@ -15,6 +15,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
@@ -34,26 +35,43 @@ public class MainUIController implements Initializable {
     private double ballY;
     private double ballRadius;
 
+    private double paddleX;
+    private double paddleY;
+    private double paddleWidth;
+    private double paddleHeight;
+    private double targetX;
+    
     private double vX;
     private double vY;
 
+    private double pX;
+    
     private double dt;
 
     private GraphicsContext gc;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        dt = .01;
+        dt = 0.01;
         ballRadius = 50;
-        vX = 50;
-        vY = 20;
+        paddleHeight = 20;
+        paddleWidth = 120;
+        
+        vX = 100;
+        vY = 50;
 
+        pX = 0;
+        
         canvasWidth = gameCanvas.getWidth();
         canvasHeight = gameCanvas.getHeight();
 
         ballX = canvasWidth / 2 - ballRadius / 2;
         ballY = canvasHeight / 2 - ballRadius / 2;
 
+        paddleX = canvasWidth / 2 - paddleWidth / 2;
+        paddleY = canvasHeight - paddleHeight;
+        targetX = paddleX;
+        
         gc = gameCanvas.getGraphicsContext2D();
         Timeline timeline = new Timeline();
         KeyFrame keyFrame = new KeyFrame(Duration.seconds(dt), event -> {
@@ -61,16 +79,32 @@ public class MainUIController implements Initializable {
             gc.fillRect(0, 0, canvasWidth, canvasHeight);
             gc.strokeRect(0, 0, canvasWidth, canvasHeight);
             gc.strokeOval(ballX, ballY, ballRadius, ballRadius);
-
+            gc.strokeRect(paddleX, paddleY, paddleWidth, paddleHeight);
+            
             if (ballX + ballRadius >= canvasWidth || ballX <= 0)
                 vX = -vX;
+            if (ballY + ballRadius >= canvasHeight || ballY <= 0)
+                vY = -vY;
+            if (Math.abs(targetX - paddleX) < 1)
+                pX = 0;
+            
             ballX = ballX + vX * dt;
             ballY = ballY + vY * dt;
+            
+            paddleX = paddleX + pX * dt;
             
         });
         timeline.getKeyFrames().add(keyFrame);
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
+    }
+
+    @FXML
+    private void handleMouseClicked(MouseEvent event) {
+        targetX = event.getX();
+        double dx = targetX - paddleX;
+        double t = 2;
+        pX = dx / t;
     }
 
 }
