4eb43140fa478f5f76fbfe9717e540fad5fd541b
kmhasan <kmhasan@SEUPC0719>
2015-12-13 12:51:25 +0600

    Client Server Demo

---
+++ Section 2/Client Server Console Demo/src/client/server/console/demo/Client.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client.server.console.demo;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author kmhasan
+ */
+public class Client {
+    public Client() {
+        try {
+            Socket socket = new Socket("172.17.0.119", 3668);
+            System.out.printf("Connected with the server\n");
+            
+            OutputStream out = socket.getOutputStream();
+            String message = "ja khushi";
+            // NEED TO TALK ABOUT UNICODE AND BYTES
+            out.write(message.getBytes());
+            out.flush();
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
