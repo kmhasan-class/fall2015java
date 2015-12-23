eca8d2bc448d4ab6f6a8ceb81a421dbdad2f622d
kmhasan <kmhasan@SEUPC0719>
2015-12-23 09:56:35 +0600

    Chat Client

---
+++ Section 3/Chat Client/src/chat/client/Client.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chat.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author kmhasan
+ */
+public class Client {
+    final String SERVER_ADDRESS = "172.17.0.119";
+    final int PORT_NUMBER = 9867;
+    
+    public Client() {
+        Socket socket;
+        String message;
+        OutputStream out;
+        String username;
+        String inputLine;
+        BufferedReader stdin;
+        
+        try {
+            username = "John Doe";
+            socket = new Socket(SERVER_ADDRESS, PORT_NUMBER);
+            out = socket.getOutputStream();
+            message = "/USERNAME:" + username;
+            out.write(message.getBytes());
+            
+            stdin = new BufferedReader(new InputStreamReader(System.in));
+            while ((inputLine = stdin.readLine()) != null) {
+                if (inputLine.equals("/EXIT"))
+                    break;
+                message = "/MESSAGE:" + inputLine;
+                out.write(message.getBytes());
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
